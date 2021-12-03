62a4dc0d78371619c26036a621fd8838df322dcc
deppong <30512867+deppong@users.noreply.github.com>
2021-12-02 21:02:05 -0500

    Edited stage 1, added stage 2

--- TTT.py
+++ TTT.py
@@ -1,8 +1,47 @@
 # Isaiah Deppong
 #
 
-board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    
 rows = len(board)
 cols = len(board[0])
-for b in board:
-    print(b)
+turn = 0
+
+
+def printboard():
+    for i in range(0, 9, 3):
+        print(f'{board[i]}, {board[i+1]}, {board[i+2]}')
+
+
+def whosturn():
+    if turn % 2 == 0:
+        return 0
+    else:
+        return 1
+
+
+def taketurn():
+    while True:
+        try:
+            position = int(input("Enter move (0-8): "))
+            if board[position] == '-':
+                if whosturn() == 0:
+                    board[position] = 'X'
+                else:
+                    board[position] = 'O'
+                break
+            else:
+                print("Spot already taken!")
+        except:
+            print("Please enter valid move")
+
+
+
+
+printboard()
+taketurn()
+turn += 1
+printboard()
+taketurn()
+turn += 1
+printboard()
